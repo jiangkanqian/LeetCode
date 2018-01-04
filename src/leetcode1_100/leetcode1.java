03ec8c5215e9b906096120031f842dbad10829be
jiangkanqian <jiangkanqian@CJ-Jkqww-NB.cjhx.wan>
2018-01-04 17:25:39 +0800

    ll

--- src/leetcode1_100/leetcode1.java
+++ src/leetcode1_100/leetcode1.java
@@ -1,5 +1,8 @@
 package leetcode1_100;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class leetcode1 {
 	
 	 public static int[] twoSum(int[] nums, int target) {
@@ -16,7 +19,35 @@ public class leetcode1 {
 	      }
 	      return res;
 	 }
-	
+	 
+	 //利用hashmap,空间换时间，时间复杂度将为O(n),空间复杂度增为O(n)
+	 public int[] twoSum1(int[] nums, int target) {
+		    Map<Integer, Integer> map = new HashMap<>();
+		    for (int i = 0; i < nums.length; i++) {
+		        map.put(nums[i], i);
+		    }
+		    for (int i = 0; i < nums.length; i++) {
+		        int complement = target - nums[i];
+		        if (map.containsKey(complement) && map.get(complement) != i) {
+		            return new int[] { i, map.get(complement) };
+		        }
+		    }
+		    throw new IllegalArgumentException("No two sum solution");
+		}
+	 
+	 //与上一种方法相比，只需要一次循环即可
+	 public int[] twoSum2(int[] nums, int target) {
+		    Map<Integer, Integer> map = new HashMap<>();
+		    for (int i = 0; i < nums.length; i++) {
+		        int complement = target - nums[i];
+		        if (map.containsKey(complement)) {
+		            return new int[] { map.get(complement), i };
+		        }
+		        map.put(nums[i], i);
+		    }
+		    throw new IllegalArgumentException("No two sum solution");
+		}
+	 
 	 public static void main(String[] args){
 		 int[] nums = {3,2,4};
 		 int target = 6;
