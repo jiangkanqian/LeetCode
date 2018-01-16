ebb13a41d38d086fd5ccc9478fca0618b628c598
jiangkanqian <jiangkanqian@CJ-Jkqww-NB.cjhx.wan>
2018-01-16 13:47:58 +0800

    ff

--- leetcode1_100/leetcode1_10/Leetcode2.java
+++ leetcode1_100/leetcode1_10/Leetcode2.java
@@ -70,6 +70,27 @@ public class Leetcode2 {
 		}
 	}
 	
+	//思路清晰，更小的空间复杂度
+	public static ListNode addTwoNumbers1(ListNode l1, ListNode l2) {
+	    ListNode dummyHead = new ListNode(0);
+	    ListNode p = l1, q = l2, curr = dummyHead;
+	    int carry = 0;
+	    while (p != null || q != null) {
+	        int x = (p != null) ? p.val : 0;
+	        int y = (q != null) ? q.val : 0;
+	        int sum = carry + x + y;
+	        carry = sum / 10;
+	        curr.next = new ListNode(sum % 10);
+	        curr = curr.next;
+	        if (p != null) p = p.next;
+	        if (q != null) q = q.next;
+	    }
+	    if (carry > 0) {
+	        curr.next = new ListNode(carry);
+	    }
+	    return dummyHead.next;
+	}
+	
 	public static void main(String[] args){
 		
 		ListNode l1 = new ListNode(1);
@@ -77,7 +98,7 @@ public class Leetcode2 {
 		l1.next = l11;
 		ListNode l2 = new ListNode(0);
 	
-		ListNode res = addTwoNumbers(l1,l2);
+		ListNode res = addTwoNumbers1(l1,l2);
 		print(res);
 	}
 	
